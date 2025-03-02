3f70da5ff354ff3477dec451f7d84980d44614f0
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-02 02:46:53 -0500

    Weird Issues
    Trying to figure out why GSAP won't work at all.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const train = document.querySelector(".train");
-
 document.addEventListener("DOMContentLoaded", (event) => {
     
     //Animation for the train
-    gsap.to(train, {
+    gsap.from('.trainMove', {
         duration: 3,
         repeat: -1,
         ease: "bounce",
         x: -20,
         yoyo: true
-    })
+    });
 });
+
+//Debugging
+console.log(document.querySelector('.trainMove'));
