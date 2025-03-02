1a5857c05973b81c2d01e824cdb5891ac3d6f593
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-02 02:23:04 -0500

    -

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const train = document.querySelector(".train");
+
 document.addEventListener("DOMContentLoaded", (event) => {
+    
     //Animation for the train
-    gsap.from('#train', {
+    gsap.to(train, {
         duration: 3,
         repeat: -1,
         ease: "bounce",
         x: -20,
-        yoyo: true,
-    });
+        yoyo: true
+    })
 });
