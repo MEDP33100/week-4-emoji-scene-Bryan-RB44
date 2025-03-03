4e73ca7da6e8a1910a1d77c3045d1abfb8296858
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-03 02:33:03 -0500

    MAJOR Changes
    - Finally made the darn train move
    - Cloud moves across screen
    - Mountain moves across screen
    TODO:
    - Make the train not break when on smaller screens
    - Make several clouds and mountains appear in the background
    - Maybe a background color?
    - Maybe more emojis??

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     
     //Animation for the train
-    gsap.from('.trainMove', {
-        duration: 3,
+    gsap.to('.trainThing', {
+        duration: 15,
         repeat: -1,
-        ease: "bounce",
-        x: -20,
-        yoyo: true
+        x: 100,
+        yoyo: true,
     });
+
+    gsap.fromTo(".mountainThing", 
+        { x: -100 },
+        { x: "100vw", duration: 5, repeat: -1, ease: "linear" }
+    );
+
+    gsap.fromTo(".cloudThing", 
+        { opacity: 0, x: 0 },
+        { opacity: 1, x: "100vw", duration: 10, repeat: -1, ease: "linear" }
+    );
 });
 
 //Debugging
-console.log(document.querySelector('.trainMove'));
