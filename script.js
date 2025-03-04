29153bb3c7d7f284d26decb45e6571a6bdeb8ef4
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-03 22:50:30 -0500

    Things Looking Good

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.to('.trainThing', {
         duration: 15,
         repeat: -1,
-        x: 100,
+        x: 150,
         yoyo: true,
     });
 
@@ -16,6 +16,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.fromTo(".cloudThing", 
         { opacity: 0, x: 0 },
         { opacity: 1, x: "100vw", duration: 10, repeat: -1, ease: "linear" }
+    );
+
+    gsap.fromTo(".treeThing", 
+        { x: -100 },
+        { x: "100vw", duration: 4, repeat: -1, ease: "linear" }
+    );
+
+    gsap.fromTo(".grassThing", 
+        { x: -100 },
+        { x: "100vw", duration: 4.5, repeat: -1, ease: "linear" }
     );
 });
 
